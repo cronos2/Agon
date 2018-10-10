65a1a9851f5078c8ddaa7a74ee352d9d1c9d4c5c
Nacho Mas <nacho.cronos2@gmail.com>
2018-10-10 12:59:27 +0200

    Updates the client and backend interfaces -- it works now!. Regarding #4

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const GameRoom = require('./src/rooms.js').GameRoom;
+const Player = require('./src/player.js');
+const Server = require('./src/server.js')(io);
+const server = new Server();
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/client.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat.message', (msg) => {
-        io.emit('chat.message', msg);
-    });
+    socket.player = new Player(socket);
+
+    socket.on(
+        'findMatch', gameName => {
+            console.log('find match');
+            server.handleNewPlayer(socket.player, gameName);
+        }
+    );
+
+    socket.on(
+        'move', move => {
+            console.log('move', move);
+            const exit_code = socket.player.gameRoom.move(socket.player, move);
+
+            if(exit_code == GameRoom.signals.EOG){
+                server.disbandRoom(socket.player.gameRoom);  // TODO
+            }
+        }
+    );
+
+    socket.on(
+        'disconnect', socket => server.handleDisconnectedPlayer(socket.player)
+    );
 });
 
-http.listen(3000, () => console.log('lol'));
+http.listen(3000);
