0ab5e14a02f93d539de3789960b81466b9faac1d
Nacho Mas <nacho.cronos2@gmail.com>
2018-10-25 17:44:56 +0200

    HTTP requests are now served status information. Closes #12.

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ require('dotenv').config();
 const http = require('http');
 const socketio = require('socket.io');
 
-const app = http.createServer(function(){});
-const io = socketio(app);
+const app = http.createServer((req, res) => {
+    const playersCount = server.lobby.players.length + server.gameRooms.reduce(
+        (count, room) => count + room.players.length,
+        0
+    );
 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        'status': 'OK',
+        'rooms': server.gameRooms.length,
+        'players': playersCount
+    }));
+});
+const io = socketio(app);
 const Server = require('./src/server.js');
 
 
